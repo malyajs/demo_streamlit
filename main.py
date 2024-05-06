a45aaadb94dbf5ac0319d81a16145532e7cd9fcc
Malyaj <169060742+malyajs@users.noreply.github.com>
2024-05-06 22:12:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import streamlit as st
-import pandas as pd
-
-
-st.title('my first demo stramlit app !!')
+#import pandas as pd
 
 regions = ['North', 'East', 'West', 'South']
 codes = ['N', 'E', 'W', 'S']
+code_dict = {r:c for r,c in zip(regions, codes)}
 
+st.title('my first demo stramlit app !!')
 # Selection box 
 # first argument takes the titleof the selectionbox
 # second argument takes options
 
 region = st.selectbox("Region: ",
                      regions)
- 
-code = {r:c for zip(regions, codes)}
-
-regions = ['North', 'East', 'West', 'South']
-codes = ['N', 'E', 'W', 'S']
- 
-code_dict = {r:c for r,c in zip(regions, codes)}
 
 st.write(f"Your region code is: {code_dict['region']}")
