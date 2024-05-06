0fa5342c3734c3d63f472cbebec6d45faa2c8a29
Malyaj <169060742+malyajs@users.noreply.github.com>
2024-05-06 22:10:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,22 @@ import pandas as pd
 
 
 st.title('my first demo stramlit app !!')
+
+regions = ['North', 'East', 'West', 'South']
+codes = ['N', 'E', 'W', 'S']
+
+# Selection box 
+# first argument takes the titleof the selectionbox
+# second argument takes options
+
+region = st.selectbox("Region: ",
+                     regions)
+ 
+code = {r:c for zip(regions, codes)}
+
+regions = ['North', 'East', 'West', 'South']
+codes = ['N', 'E', 'W', 'S']
+ 
+code_dict = {r:c for r,c in zip(regions, codes)}
+
+st.write(f"Your region code is: {code_dict['region']}")
